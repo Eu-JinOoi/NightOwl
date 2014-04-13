8de48eabf749402fef35c18144925d801009c247
Eu-Jin Ooi <BNSF4924@gmail.com>
2014-04-13 00:22:41 -0700

    About Page | Almost done ~

--- pages/about.php
+++ pages/about.php
@@ -10,24 +10,43 @@
 		max-width:200px;
 		display:inline;
 	}
+	ul
+	{
+		display:inline;	
+		list-style-type: none;
+		margin:0;
+		padding:0;
+	}
+	ul li
+	{
+		display:inline;	
+		max-width:200px;
+		text-decoration:none;	
+	}
+	li h2
+	{
+		display:inline;
+		margin:none;
+	}
 </style>
 <div style='margin:auto; max-width:90%; text-align:center; position:relative;'>
 	<h1 style="color:#000; display:block; margin-bottom:0px;">Who We Are</h1>
     <hr style='width:20%'>
     <div style='margin:auto; text-align:center; position:relative;'>
-        <div class='person'>
+    <ul>
+    	<li>
             <img src='/resources/images/about/bryanyoshinaga.png' style='width:100px;'>
             <h2 style="color:#000;">Bryan Yoshinaga</h2>
-        </div>
-        <div class='person'>
+        </li>
+       	<li>
             <img src='/resources/images/about/eu-jinooi.png' style='width:100px;'>
             <h2 style="color:#000;">Eu-Jin Ooi</h2>
-        </div>
-         <div class='person'>
+        </li>
+        <li>
             <img src='/resources/images/about/kendallreiter.png' style='width:100px;'>
             <h2 style="color:#000;">Kendall Reiter</h2>
-        </div>
-        <div style='clear:both;'>&nbsp;</div>
+        </li>
+     </ul>
     </div>
     <div style='float:left;'>&nbsp;</div>
 </div>
