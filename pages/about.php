404948dd8f36b1c0419e9ca44cd6a09fa77534c2
Eu-Jin Ooi <BNSF4924@gmail.com>
2014-04-13 00:14:14 -0700

    About Page   Div seperators

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,33 @@
-<div style='margin:auto; width:90%; text-align:center;'>
+<style>
+	.person
+	{
+		/*float:left;*/
+		margin:auto;
+		padding:5px;
+		width:30%;
+		position:relative;
+		text-align:center;
+		max-width:200px;
+		display:inline;
+	}
+</style>
+<div style='margin:auto; max-width:90%; text-align:center; position:relative;'>
 	<h1 style="color:#000; display:block; margin-bottom:0px;">Who We Are</h1>
     <hr style='width:20%'>
-    
-    
+    <div style='margin:auto; text-align:center; position:relative;'>
+        <div class='person'>
+            <img src='/resources/images/about/bryanyoshinaga.png' style='width:100px;'>
+            <h2 style="color:#000;">Bryan Yoshinaga</h2>
+        </div>
+        <div class='person'>
+            <img src='/resources/images/about/eu-jinooi.png' style='width:100px;'>
+            <h2 style="color:#000;">Eu-Jin Ooi</h2>
+        </div>
+         <div class='person'>
+            <img src='/resources/images/about/kendallreiter.png' style='width:100px;'>
+            <h2 style="color:#000;">Kendall Reiter</h2>
+        </div>
+        <div style='clear:both;'>&nbsp;</div>
+    </div>
+    <div style='float:left;'>&nbsp;</div>
 </div>
