78986179ae4eaee7acdfa9d2d8ab9217b46fee8e
Eu-Jin Ooi <BNSF4924@gmail.com>
2014-04-22 04:18:33 -0700

    Changed "Search" button to work better with menu open

--- index.php
+++ index.php
@@ -23,13 +23,17 @@
 			{
 				//$("#menu").css("left","0%");
 				$("#menu").stop().animate({"left":"0%"});
-				$("#searchRegion").stop().animate({"padding-left":"215px"});
+				$("#searchRegion").stop().animate({"padding-left":"210px"});
+				$("#doSearch").css({"padding-left":"5px","padding-right":"5px"});
+				$("#doSearch").html("<img src='search.png' style='max-height:12px;'/>");
 				menuState=1;
 			}
 			else
 			{
 				$("#menu").stop().animate({"left":"-65%"});
 				$("#searchRegion").stop().animate({"padding-left":"15px"});
+				$("#doSearch").css({"padding-left":"10px","padding-right":"10px"});
+				$("#doSearch").html("Search");
 				menuState=0;  
 			}
 	}
