558965b9c59382ee4056f4b4d69046d9910e3bdb
Eu-Jin Ooi <BNSF4924@gmail.com>
2016-01-18 03:30:05 -0800

    Added tags to the site to work as filters. Additionally, changed display to show 3 columns when the display is large instead of 2.

--- v2/index.php
+++ v2/index.php
@@ -18,8 +18,13 @@
 </header>
 <main>
    	<div class="container">
+    	<div class="row" style="margin-top:10px;"><!--Tags-->
+       		<div class="chip">Open Now<i class="material-icons">close</i></div>
+            <div class="chip">Other Filtering Categories<i class="material-icons">close</i></div>
+            <div class="chip">Restaurants<i class="material-icons">close</i></div>
+        </div>
 		<div class="row">
-        	<div class='col s12 m6 l4'>
+        	<div class='col s12 m6 l6'>
             	<div class="card-panel light-blue lighten-5">
                 	<span class="">I am a very simple card. I am good at containing small bits of information.
                   I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
@@ -42,27 +47,22 @@
 					</span>
                 </div>
             </div>
-            <div class='col s12 m6 l4'>
+            <div class='col s12 m6 l6'>
             	<div class="card-panel teal">
                 	<span class="white-text">I am a very simple card. I am good at containing small bits of information.
                   I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
 					</span>
                 </div>
             </div>
-            <div class='col s12 m6 l4'>
-            	<div class="card-panel">
-                	<span class="blue-text">I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-					</span>
-                </div>
-            </div>
         </div>
     </div>
-    <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
-    <a class="btn-floating btn-large red">
-      <i class="large material-icons">search</i>
-    </a>
-  </div>
+    <div class="container">
+	    <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+    		<a class="btn-floating btn-large red">
+		    <i class="large material-icons">search</i>
+    		</a>
+  		</div>
+	</div>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="/v2/resources/materialize/js/materialize.min.js"></script>
